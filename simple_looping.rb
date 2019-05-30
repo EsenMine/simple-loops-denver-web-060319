09fbd254bbae96b9dbd2f6171a7e93bc3370b462
Miné E <mine_esen@yahoo.ca>
2019-05-30 00:58:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,8 +4,8 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
-    counter += 1
     break if counter == number_of_times
+    counter += 1
   end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer
